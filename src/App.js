26b38f0c1d85515fb192c5581963bcca17b4c431
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2019-08-03 22:50:26 -0700

    add setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import SocketIOClient from 'socket.io-client';
 import GoogleMap from './components/googleMap';
 import Time from './components/time';
 import Weather from './components/weather'
-import googleMap from './components/googleMap';
 import './components/css/mirror.css';
 
+const electron = window.require('electron');
+const ipcRenderer  = electron.ipcRenderer;
+
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = { 
+          WeatherConfig: "OFF",
+         MapConfig: "OFF", 
+         NewsConfig: "OFF", 
+         DateConfig: "OFF", 
+         DeviceID:""
+        }
+        this.socket = SocketIOClient('ec2-18-212-195-64.compute-1.amazonaws.com', { transports: ['websocket'] });
+  }
+
+  componentDidMount(){
+    ipcRenderer.send('mac:get','get')
+    ipcRenderer.on('mac:send',(event,mac)=>{
+
+      axios.get('http://ec2-18-212-195-64.compute-1.amazonaws.com/api/configDisplay',{params:{DeviceID:mac}}).then(res=>{
+        console.log(res.data)
+        this.setState(res.data)
+        this.socket.emit('config:receive',{ config: {
+          DeviceID:res.data.DeviceID
+        }})
+      }).catch(err=>console.log(err))
+    })
+
+      this.socket.on('config:send',(data)=>{
+        console.log(data)
+        this.setState(data.config)
+      })
+  }
+
   render() {
     return (
       <div>
