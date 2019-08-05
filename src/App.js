62db37464d623d4219309a8914534facc3a220b2
Alex Jiang <it.jinshan@gmail.com>
2019-08-05 13:30:29 -0700

    working google map

--- src/App.js
+++ src/App.js
@@ -3,49 +3,9 @@ import GoogleMap from './components/googleMap';
 import './components/googleMap.css';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      middle: false,
-      right: true,
-    };
-  }
-
-  showGoogleMap(){
-    if(this.state.right === true){
-      return(
-        <div className="row-12">
-          <div className="col-9"></div>
-          <div className="col-3"><GoogleMap /></div>
-        </div>
-      )
-    }
-    else{
-      return(
-          <div className="row-12">
-            <div className="col-3"><GoogleMap /></div>
-            <div className="col-9"></div>
-          </div>
-      )
-    }
-  }
-
   render() {
     return (
-      <div>
-        <div style={{ color:'white'}}>
-          Import component here!
-        </div>
-        { this.state.middle === true ? (
-          <div className="maps-middle-row">
-            { this.showGoogleMap() }
-          </div>
-        ) :(
-          <div className="maps-bottom-row">
-            { this.showGoogleMap() }
-          </div> 
-        )}
-      </div>
+      <div><GoogleMap /></div>
     );
   }
 }
