1003e008355d092a1f938989948879d4620fc9a5
kev-96 <kev-96@users.noreply.github.com>
2019-11-03 19:43:18 -0800

    Update googleMap.js

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -4,10 +4,12 @@ import { withScriptjs,
          withGoogleMap, 
          GoogleMap, 
          Marker,
-         TrafficLayer } from "react-google-maps"
+         TrafficLayer,
+        DirectionsRenderer } from "react-google-maps"
 
 var userLat; //= 37.3352;
 var userLng; //= -121.8811;
+var wAddress = "San Jose State University";
 
 const defaultMapOptions = {
     fullscreenControl: false,
@@ -37,22 +39,49 @@ const MapWithATrafficLayer = compose(
   );
 
 export class googleMaps extends Component {
+  state = {
+    directions: null
+  }
+  
   getLocation = () =>{
     return new Promise(function (resolve, reject){
         navigator.geolocation.getCurrentPosition(resolve, reject);
     });
   }
+
   componentDidMount(){
     this.getLocation()
     .then((position)=>{
       userLat = position.coords.latitude
       userLng = position.coords.longitude
     })
+    const directionsService = new google.maps.DirectionsService();
+
+    const origin = { lat: userLat, lng: userLng };
+    const destination = wAddress;
+
+    directionsService.route(
+      {
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          this.setState({
+            directions: result
+          });
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      }
+    );
   }
     render(){
         return(
               <div id='maps'
                    style={{alignitems:'right', justifyContent:'right'}}>
+                     ETA {this.state.directions}
                 <MapWithATrafficLayer />
               </div>
         )
