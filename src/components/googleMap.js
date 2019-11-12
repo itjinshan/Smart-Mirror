1bc4893c5cb7620ae33ec1ca4856b4ed3c3e1303
Alex Jiang <it.jinshan@gmail.com>
2019-11-11 19:01:01 -0800

    map & eta height adjustment

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -21,7 +21,7 @@ const MapWithATrafficLayer = compose(
     withProps({
       googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBjtRUvjcEnZpsmS4xtRF1f5HZ1RRV8qWI&v=3.exp&libraries=geometry,drawing,places",
       loadingElement: <div style={{ height: '100%' }} />,
-      containerElement: <div style={{ height: '340px' }} />,
+      containerElement: <div style={{ height: '300px' }} />,
       mapElement: <div style={{ height: '100%' }} />,
     }),
     withScriptjs,
