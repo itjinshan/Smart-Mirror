b1a0fe357d39fd07ec394a4e387df1c97996d4d1
HongbinZheng <36983590+HongbinZheng@users.noreply.github.com>
2019-11-20 02:19:08 -0800

    fetch address

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ class App extends Component {
           {this.state.MapConfig === 'middle-left'?(
               <div className="row">
                 <div className='col-8'>
-                  <GoogleMap className='row'/>
+                  <GoogleMap className='row' address={this.state.Address}/>
                 </div>
                 <div className="col-4"></div>
               </div>
@@ -169,7 +169,7 @@ class App extends Component {
               <div className="row">
                 <div className="col-4"></div>
                 <div className='col-8'>
-                  <GoogleMap className='row'/>
+                  <GoogleMap className='row' address={this.state.Address}/>
                 </div>
               </div>
             ): null}
@@ -197,7 +197,7 @@ class App extends Component {
           {this.state.MapConfig === 'bottom-left'?(
               <div className="row">
                 <div className='col-8'>
-                  <GoogleMap className='row'/>
+                  <GoogleMap className='row'  address={this.state.Address}/>
                 </div>
                 <div className="col-4"></div>
               </div>
@@ -214,7 +214,7 @@ class App extends Component {
           <div id='center' className = 'col-4 text-center'>
           {this.state.MapConfig === 'bottom-middle'?(
               <div>
-                <GoogleMap className='row'/>
+                <GoogleMap className='row' address={this.state.Address}/>
               </div>
             ): null}
           </div>
@@ -231,7 +231,7 @@ class App extends Component {
               <div className="row">
                 <div className="col-4"></div>
                 <div className="col-8">
-                  <GoogleMap className='row'/>
+                  <GoogleMap className='row' address={this.state.Address}/>
                 </div>
               </div>
             ): null}
